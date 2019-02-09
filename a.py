2580b260e6382c0a329030eaf16526a3ecaa0f15
udogangul <udogangul@gmail.com>
2019-02-09 19:15:27 +0100

    Face Detect test

--- a.py
+++ a.py
@@ -1,5 +1,8 @@
 import cv2
 
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
 webcam = cv2.VideoCapture(0)
 
 while True:
@@ -13,7 +16,19 @@ while True:
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    cv2.imshow('Gray Window', gray)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+       # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+
+    cv2.imshow('Window', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
